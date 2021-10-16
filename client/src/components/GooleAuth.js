2240b3f2d8d69267286c3962c532f1434830439c
JanAwadallah <jan.awadallah@gmail.com>
2021-10-17 02:58:25 +1100

    Modification

--- client/src/components/GooleAuth.js
+++ client/src/components/GooleAuth.js
@@ -146,6 +146,7 @@ const GoogleAuth = () => {
     let fetchedAddress = "";
     const { latitude, longitude } = pos.coords;
     const key = process.env.REACT_APP_GEOCODE_KEY;
+    console.log(key);
     fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${key}`
     )
