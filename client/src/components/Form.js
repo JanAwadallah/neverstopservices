cbfbb01c47e543dbd74828c3b7f4bac9ac864d44
JanAwadallah <32688971+JanAwadallah@users.noreply.github.com>
2021-10-20 13:36:52 +1100

    Update Form.js

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button, Form } from "semantic-ui-react";
 
-const CheckForm = ({ onSubmit, address, checkStatus, date, time }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+const CheckForm = ({
+  onSubmit,
+  address,
+  checkStatus,
+  date,
+  time,
+  user,
+  error,
+  setError,
+}) => {
+  const [currentUser, setCurrentUser] = useState(user);
+  useEffect(() => {
+    errorDiv();
+  }, [currentUser]);
+
+  const errorDiv = (field) => {
+    if (error) {
+      return (
+        <div className="ui negative message center aligned segment ">
+          <h4>{field} can not be blanck</h4>
+        </div>
+      );
+    } else {
+      return;
+    }
+  };
+
   return (
     <div
       style={{
@@ -10,38 +36,44 @@ const CheckForm = ({ onSubmit, address, checkStatus, date, time }) => {
       }}
     >
       <Form className="ui container">
-        <Form.Field>
+        <Form.Field required>
           <label style={{ fontSize: 20 }}>Full Name</label>
           <input
+            id="fullname"
             value={currentUser}
             onChange={(e) => {
               setCurrentUser(e.target.value);
+              setError(false);
             }}
             placeholder="Please enter your Full Name      أكتب الاسم بالكامل"
           />
+          {errorDiv("Name")}
         </Form.Field>
-        <Form.Field>
+        <Form.Field required>
           <label style={{ fontSize: 20 }}>Address</label>
-          <input value={address} />
+          <input required value={address} />
         </Form.Field>
-        <Form.Field>
+        <Form.Field required>
           <label style={{ fontSize: 20 }}>Date</label>
-          <input value={date} />
+          <input required value={date} />
         </Form.Field>
-        <Form.Field>
+        <Form.Field required>
           <label style={{ fontSize: 20 }}>Time</label>
-          <input value={time} />
+          <input required value={time} />
         </Form.Field>
 
         <Button
           style={{ fontSize: 20 }}
           className="fluid ui button"
           type="submit"
-          onClick={(user) => onSubmit(currentUser)}
+          onClick={() => {
+            onSubmit(currentUser);
+          }}
         >
           {checkStatus === "IN" ? "Check in" : "Check out"}
         </Button>
       </Form>
+      <Link to="/list">List</Link>
     </div>
   );
 };
